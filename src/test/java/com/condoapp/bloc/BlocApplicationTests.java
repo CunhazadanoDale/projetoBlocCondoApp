e1b5a98212af7e84c293c609467b5b6903d5dc32
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-11 22:36:00 -0300

    feat: base do projeto

---
+++ src/test/java/com/condoapp/bloc/BlocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condoapp.bloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
