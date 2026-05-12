065540cd3be4f0c2f453bcaa6854da21f3931617
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-12 20:52:28 -0300

    feat: classe utilitaria para indexar endereco nas entidades

--- src/main/java/com/condoapp/bloc/util/embedded/Endereco.java
+++ src/main/java/com/condoapp/bloc/util/embedded/Endereco.java
@@ -1,4 +1,23 @@
 package com.condoapp.bloc.util.embedded;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Endereco {
+
+    private String cep;
+    private String nomeRua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String numero;
+    private String complemento;
 }
