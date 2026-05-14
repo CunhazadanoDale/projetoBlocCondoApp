628c5cce60829d96381b7b9928e10f80c4fef6f0
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-13 21:12:48 -0300

    feat: versionalizado codigo banco de dados com conta presente

--- src/main/resources/db/migration/V1__create_condominio_conta.sql
+++ src/main/resources/db/migration/V1__create_condominio_conta.sql
@@ -1,13 +1,24 @@
 CREATE TABLE tb_condominio (
-id BIGSERIAL PRIMARY KEY,
+condominio_id BIGSERIAL PRIMARY KEY,
 uuid UUID NOT NULL UNIQUE,
 nome_condominio VARCHAR(60) NOT NULL,
 cnpj_condominio VARCHAR(14) NOT NULL UNIQUE,
-cep TEXT, nome_Rua TEXT, bairro TEXT, cidade TEXT, estado TEXT, numero TEXT, complemento TEXT,
+cep TEXT, nome_rua TEXT, bairro TEXT, cidade TEXT, estado TEXT, numero TEXT, complemento TEXT,
 telefone_condominio TEXT,
 email_sindico TEXT,
 senha_atual TEXT,
 senha_expira_em TIMESTAMP,
 ativo BOOLEAN,
 criado_em TIMESTAMP DEFAULT now()
-)
+);
+
+
+CREATE TABLE tb_conta(
+conta_id BIGSERIAL PRIMARY KEY,
+uuid UUID NOT NULL UNIQUE,
+email TEXT NOT NULL UNIQUE,
+senha_hash TEXT NOT NULL,
+role TEXT,
+ativo BOOLEAN,
+criado_em TIMESTAMP DEFAULT now()
+);
