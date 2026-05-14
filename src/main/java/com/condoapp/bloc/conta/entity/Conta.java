1a220904e612c3c6be6ca8d14818cbff40338085
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-13 21:12:29 -0300

    feat: entidade conta sem relacionamento presete (ainda) com morador ou prestador

---
+++ src/main/java/com/condoapp/bloc/conta/entity/Conta.java
@@ -0,0 +1,42 @@
+package com.condoapp.bloc.conta.entity;
+
+import com.condoapp.bloc.conta.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_conta")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contaId;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID uuid;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String senhaHash;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean ativo;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+}
---
+++ src/main/java/com/condoapp/bloc/conta/enums/Role.java
@@ -0,0 +1,6 @@
+package com.condoapp.bloc.util.enums;
+
+public enum Role {
+    MORADOR,
+    PRESTADOR
+}
