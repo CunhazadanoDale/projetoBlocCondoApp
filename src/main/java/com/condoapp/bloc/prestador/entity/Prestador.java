01dc3ea01ca39a7381a197bc3b2fab69d6ae975d
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-14 21:25:06 -0300

    feat: adição banco de dados prestador com relacções configuradas

--- src/main/java/com/condoapp/bloc/prestador/entity/Prestador.java
+++ src/main/java/com/condoapp/bloc/prestador/entity/Prestador.java
@@ -1,4 +1,56 @@
 package com.condoapp.bloc.prestador.entity;
 
+import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.prestador.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_prestador")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Prestador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long prestadorId;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID uuid;
+
+    @OneToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+    @Column(nullable = false, length = 60)
+    private String nomeCompleto;
+
+    @Column(unique = true, length = 20)
+    private String cpfOuCnpj;
+
+    private String telefone;
+
+    @Column(nullable = true)
+    private String descricao;
+
+    private BigDecimal saldo;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(nullable = true)
+    private BigDecimal avaliacaoMedia;
+
+    private Integer totalAvaliacoes;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
 }
