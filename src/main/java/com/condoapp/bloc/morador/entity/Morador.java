4f6c8468569ca2e0366a17db8c31afd4bfd1a9e6
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-14 21:20:44 -0300

    feat: criação da entidade morador com relacionamento com conta e condominio

--- src/main/java/com/condoapp/bloc/morador/entity/Morador.java
+++ src/main/java/com/condoapp/bloc/morador/entity/Morador.java
@@ -1,4 +1,51 @@
 package com.condoapp.bloc.morador.entity;
 
+import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.condominio.entity.Condominio;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_morador")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Morador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long moradorId;
+
+    @Column(unique = true, updatable = false, nullable = false)
+    private UUID uuid;
+
+    @Column(nullable = false, length = 60)
+    private String nomeCompleto;
+
+    @OneToOne
+    @JoinColumn(name = "conta_id", nullable = true)
+    private Conta conta;
+
+    @OneToOne
+    @JoinColumn(name = "condominio_id", nullable = false)
+    private Condominio condominio;
+
+    private String unidade;
+
+    @Column(nullable = true)
+    private String bloco;
+
+    @Column(nullable = true)
+    private String telefone;
+
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
 }
