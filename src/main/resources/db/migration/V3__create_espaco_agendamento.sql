04f163ca593bc4646352add8ce6de35848e57955
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-20 20:39:21 -0300

    feat: terceira versao de migration para criar entidades

--- src/main/resources/db/migration/V3__create_espaco_agendamento.sql
+++ src/main/resources/db/migration/V3__create_espaco_agendamento.sql
@@ -10,4 +10,18 @@ cancelamento_min_horas INTEGER,
 limite_reserva_semana INTEGER,
 ativo boolean NOT NULL DEFAULT true,
 version BIGINT NOT NULL DEFAULT 0
-)
+);
+
+CREATE TABLE tb_agendamento(
+agendamento_id BIGSERIAL PRIMARY KEY,
+uuid UUID NOT NULL UNIQUE,
+espaco_id BIGINT NOT NULL REFERENCES tb_espaco,
+morador_id BIGINT REFERENCES tb_morador,
+nome_responsavel VARCHAR(60) NOT NULL,
+unidade_responsavel VARCHAR(60) NOT NULL,
+inicio TIMESTAMP NOT NULL,
+fim TIMESTAMP NOT NULL,
+status VARCHAR(30) NOT NULL CHECK(status IN('PENDENTE', 'CONFIRMADO', 'CANCELADO', 'EXPIRADO')),
+observacao TEXT,
+criado_em TIMESTAMP DEFAULT now()
+);
