f9ea5c169ada24d2df756eb5e1ed7c3be41b35f5
CunhazadaNoDale <cunha12324@gmail.com>
2026-05-20 21:17:04 -0300

    feat: migration conclida de chamado_comentario

--- src/main/resources/db/migration/V4__create_chamado_comentario.sql
+++ src/main/resources/db/migration/V4__create_chamado_comentario.sql
@@ -12,4 +12,13 @@ urgencia_chamado VARCHAR(30) NOT NULL CHECK(urgencia_chamado IN('ALTA', 'MEDIA',
 status_chamado VARCHAR(30) NOT NULL CHECK(status_chamado IN('ABERTO', 'EM_ANDAMENTO', 'RESOLVIDO', 'FECHADO')),
 criado_em TIMESTAMP NOT NULL DEFAULT now(),
 resolvido_em TIMESTAMP
+);
+
+CREATE TABLE tb_comentario_chamado(
+comentario_id BIGSERIAL PRIMARY KEY,
+chamado_id BIGINT NOT NULL REFERENCES tb_chamado,
+autor_nome VARCHAR(60) NOT NULL,
+autor_role VARCHAR(30) NOT NULL CHECK(autor_role IN('SINDICO', 'MORADOR')),
+texto TEXT NOT NULL,
+criado_em TIMESTAMP NOT NULL DEFAULT now()
 )
