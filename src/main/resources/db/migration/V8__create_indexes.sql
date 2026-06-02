993a59eec4cfafd5e4aa8eb2e74a95d8af772cf8
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-01 21:28:27 -0300

    feat: adicao dos index dos bancos de dados manualmente

---
+++ src/main/resources/db/migration/V8__create_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_prestador_status ON tb_prestador(status);
+CREATE INDEX idx_agendamento_status ON tb_agendamento(status);
+CREATE INDEX idx_chamado_status ON tb_chamado(status);
+CREATE INDEX idx_contratacao_status ON tb_contratacao(status);
+CREATE INDEX idx_saque_status ON tb_saque(status);
