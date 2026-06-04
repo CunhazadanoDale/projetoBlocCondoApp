b837b2b2a0b4df4f483e5b21db3f315e1cad90e4
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-03 23:56:56 -0300

    feat: agendamento service sem ser interface apenas para teste

--- src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
@@ -1,4 +1,21 @@
 package com.condoapp.bloc.agendamento.service;
 
-public interface AgendamentoService {
+import com.condoapp.bloc.agendamento.entity.Agendamento;
+import com.condoapp.bloc.agendamento.repository.AgendamentoRepository;
+
+import java.util.UUID;
+
+public class AgendamentoService {
+
+    private final AgendamentoRepository agendamentoRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository) {
+        this.agendamentoRepository = agendamentoRepository;
+    }
+
+
+    public Agendamento buscarPorUUID(UUID uuid) {
+        return agendamentoRepository.findByUuid(uuid)
+                .orElseThrow(() -> new RuntimeException("agendamento não encontrado"));
+    }
 }
