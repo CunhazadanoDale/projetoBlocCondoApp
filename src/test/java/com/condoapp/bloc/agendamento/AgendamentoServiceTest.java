9188872fa16e2de3ab408af0b6a00dba4254d6e9
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-04 20:42:19 -0300

    feat: adição do teste com mockito

--- src/test/java/com/condoapp/bloc/agendamento/AgendamentoServiceTest.java
+++ src/test/java/com/condoapp/bloc/agendamento/AgendamentoServiceTest.java
@@ -1,15 +1,69 @@
 package com.condoapp.bloc.agendamento;
 
 import com.condoapp.bloc.agendamento.entity.Agendamento;
+import com.condoapp.bloc.agendamento.entity.Espaco;
+import com.condoapp.bloc.agendamento.enums.StatusAgendamento;
+import com.condoapp.bloc.agendamento.repository.AgendamentoRepository;
+import com.condoapp.bloc.agendamento.service.AgendamentoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
-public class AgendamentoService {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-    private final AgendamentoRepository agendamentoRepository;
+@ExtendWith(MockitoExtension.class)
+public class AgendamentoServiceTest {
 
-    public Agendamento buscarPorUUID(UUID uuid) {
-        return agendamentoRepositor
+    @Mock
+    private AgendamentoRepository agendamentoRepository;
+
+    @InjectMocks
+    private AgendamentoService agendamentoService;
+
+    @Test
+    public void deveAcharOAgendamento() {
+
+        UUID uuiDdoTeste = UUID.randomUUID();
+
+        Mockito.when(agendamentoRepository.findByUuid(uuiDdoTeste))
+                .thenReturn(Optional.of(Agendamento.builder()
+                        .agendamentoId(1L)
+                        .uuid(uuiDdoTeste)
+                        .espaco(Espaco.builder().build())
+                        .nomeResponsavel("Cunha")
+                        .unidadeResponsavel("Cuia")
+                        .inicio(LocalDateTime.now())
+                        .fim(LocalDateTime.now())
+                        .status(StatusAgendamento.CONFIRMADO)
+                        .observacao("")
+                        .criadoEm(LocalDateTime.now())
+                        .build())
+                );
+
+        Agendamento agendamentoPorUuid = agendamentoService.buscarPorUUID(uuiDdoTeste);
+
+        assertThat(agendamentoPorUuid).isInstanceOf(Agendamento.class);
+        assertThat(agendamentoPorUuid).isNotNull();
+        assertThat(agendamentoPorUuid.getNomeResponsavel()).isEqualToIgnoringCase("cunha");
+        assertThat(agendamentoPorUuid.getUuid()).isEqualTo(uuiDdoTeste);
+    }
+
+    @Test
+    public void naoDeveEncontrarOAgendamento() {
+        UUID uuidDoTeste = UUID.randomUUID();
+
+        Mockito.when(agendamentoRepository.findByUuid(uuidDoTeste))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> agendamentoService.buscarPorUUID(uuidDoTeste)).isInstanceOf(RuntimeException.class)
+                .hasMessage("agendamento não encontrado");
     }
 }
