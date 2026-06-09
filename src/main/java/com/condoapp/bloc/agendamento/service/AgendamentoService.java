6da57b4a0aa0401d871663f85e41fb023accd43b
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-08 21:37:16 -0300

    feat: adição de mais metodos no agendamento

--- src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
@@ -2,9 +2,12 @@ package com.condoapp.bloc.agendamento.service;
 
 import com.condoapp.bloc.agendamento.entity.Agendamento;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AgendamentoService {
     Agendamento buscarPorUUID(UUID uuid);
+    Agendamento criarAgendamento(Agendamento agendamento);
+    List<Agendamento> listarAgendamentosDeCondominio(UUID condominioId);
     Agendamento cancelarAgendamento(UUID uuid);
 }
