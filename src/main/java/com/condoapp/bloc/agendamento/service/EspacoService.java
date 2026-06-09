d89853d949cd3ed2e8d4e069522b624da24ca6ef
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-08 21:37:32 -0300

    feat: adição da interface de espaco

--- src/main/java/com/condoapp/bloc/agendamento/service/EspacoService.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/EspacoService.java
@@ -1,4 +1,13 @@
 package com.condoapp.bloc.agendamento.service;
 
+import com.condoapp.bloc.agendamento.entity.Espaco;
+
+import java.util.List;
+
 public interface EspacoService {
+
+    List<Espaco> listarEspacos();
+    Espaco criarEspaco(Espaco espaco);
+    Espaco atualizarEspaco(Long espacoId, Espaco espaco);
+    Espaco gradeDeDisponibilidadeEspaco(Long espacoId);
 }
