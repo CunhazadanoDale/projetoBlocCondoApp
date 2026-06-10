1c3277b80a525a0f527628a43c10f3ff9d57f017
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-09 21:05:44 -0300

    feat: base do controller feita

--- src/main/java/com/condoapp/bloc/agendamento/controller/AgendamentoController.java
+++ src/main/java/com/condoapp/bloc/agendamento/controller/AgendamentoController.java
@@ -1,4 +1,45 @@
 package com.condoapp.bloc.agendamento.controller;
 
+import com.condoapp.bloc.agendamento.entity.Agendamento;
+import com.condoapp.bloc.agendamento.service.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/condominios")
+@RequiredArgsConstructor
 public class AgendamentoController {
+
+    private final AgendamentoService agendamentoService;
+
+    @GetMapping("/{uuid}/agendamentos")
+    public ResponseEntity<List<Agendamento>> listarAgendamentosDoCondominio(@PathVariable(name = "uuid") UUID condominioUuid) {
+        return new ResponseEntity<>(agendamentoService.listarAgendamentosDeCondominio(condominioUuid), HttpStatus.OK);
+    }
+
+    @PostMapping("/{uuid}/espacos/{espacoId}/agendamentos")
+    public ResponseEntity<Agendamento> criarReserva(@PathVariable(name = "uuid") UUID condominioUuid,
+                                                    @PathVariable Long espacoId,
+                                                    @RequestBody Agendamento agendamento
+                                                    ) {
+        return new ResponseEntity<>(agendamentoService.criarAgendamento(agendamento), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/agendamentos/{agendamentoUuid}")
+    public ResponseEntity<Agendamento> cancelarReserva(@PathVariable UUID agendamentoUuid) {
+        return new ResponseEntity<>(agendamentoService.cancelarAgendamento(agendamentoUuid), HttpStatus.OK);
+    }
+
+    @GetMapping("/{uuid}/espacos/{espacoId}/disponibilidade")
+    public ResponseEntity<List<Agendamento>> listarDisponibilidade(@PathVariable(name = "uuid") UUID condominioUuid,
+                                                                   @PathVariable Long espacoId,
+                                                                   @RequestParam LocalDate data) {
+        return new ResponseEntity<>(agendamentoService.buscarDisponibilidade(espacoId, data), HttpStatus.OK);
+    }
 }
