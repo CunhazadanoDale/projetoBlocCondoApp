1a470b1158e19941504d04999c7008751bbf3c24
CunhazadaNoDale <cunha12324@gmail.com>
2026-06-16 23:28:25 -0300

    feat: adição do service para buscar por email

--- src/main/java/com/condoapp/bloc/auth/service/ContaDetailsService.java
+++ src/main/java/com/condoapp/bloc/auth/service/ContaDetailsService.java
@@ -1,14 +1,25 @@
 package com.condoapp.bloc.auth.service;
 
+import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.auth.repository.ContaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class ContaDetailService implements UserDetailsService {
+@Service
+@RequiredArgsConstructor
+public class ContaDetailsService implements UserDetailsService {
+
+    private final ContaRepository contaRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Conta conta = contaRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Conta não encontrada"));
+
+        return conta;
     }
 
 }
