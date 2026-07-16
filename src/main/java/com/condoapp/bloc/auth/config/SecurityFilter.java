1946ca50c290966d58a7fbba754cc2be3c41a4c1
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-15 22:33:24 -0300

    feat: inicio de um security filter

---
+++ src/main/java/com/condoapp/bloc/auth/config/SecurityFilter.java
@@ -0,0 +1,24 @@
+package com.condoapp.bloc.auth.config;
+
+
+import com.condoapp.bloc.auth.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authorization authorization = ;
+    }
+}
