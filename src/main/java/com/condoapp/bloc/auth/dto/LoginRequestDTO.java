54ab35da115061c41a3137de01b7ea0a183cd302
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-20 19:38:13 -0300

    feat: DTOS de conexão

--- src/main/java/com/condoapp/bloc/auth/dto/LoginRequestDTO.java
+++ src/main/java/com/condoapp/bloc/auth/dto/LoginRequestDTO.java
@@ -1,4 +1,18 @@
 package com.condoapp.bloc.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String senha;
 }
--- src/main/java/com/condoapp/bloc/auth/dto/LoginResponseDTO.java
+++ src/main/java/com/condoapp/bloc/auth/dto/LoginResponseDTO.java
@@ -1,4 +1,12 @@
 package com.condoapp.bloc.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponseDTO {
+    private String token;
 }
