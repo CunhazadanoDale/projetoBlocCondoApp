306568e5cdb36aeca136862a3dee1f4fa80d1c5c
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-20 19:17:06 -0300

    feat: adição do security filter completo

--- src/main/java/com/condoapp/bloc/auth/config/SecurityFilter.java
+++ src/main/java/com/condoapp/bloc/auth/config/SecurityFilter.java
@@ -1,24 +1,51 @@
 package com.condoapp.bloc.auth.config;
 
 
+import com.condoapp.bloc.auth.service.ContaDetailsService;
 import com.condoapp.bloc.auth.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final ContaDetailsService contaDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Authorization authorization = ;
+        String cabecalho = request.getHeader("Authorization");
+        if (cabecalho == null || !cabecalho.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String valorToken = cabecalho.substring(7);
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            String emailFromToken = jwtService.extrairEmail(valorToken);
+            UserDetails conta = contaDetailsService.loadUserByUsername(emailFromToken);
+            boolean ehValido = jwtService.isTokenValido(valorToken, conta);
+
+            if (ehValido) {
+                UsernamePasswordAuthenticationToken autenticacao =
+                        new UsernamePasswordAuthenticationToken(conta, null, conta.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(autenticacao);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
