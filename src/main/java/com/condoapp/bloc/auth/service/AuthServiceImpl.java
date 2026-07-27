d304237d38e27d6b1f28c846ec2ab7c6c6ec690d
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-27 19:37:29 -0300

    feat: implementação do serviço interface

--- src/main/java/com/condoapp/bloc/auth/service/AuthServiceImpl.java
+++ src/main/java/com/condoapp/bloc/auth/service/AuthServiceImpl.java
@@ -1,4 +1,26 @@
 package com.condoapp.bloc.auth.service;
 
-public class AuthServiceImpl {
+import com.condoapp.bloc.auth.dto.LoginRequestDTO;
+import com.condoapp.bloc.auth.dto.LoginResponseDTO;
+import com.condoapp.bloc.auth.dto.RegisterRequestDTO;
+import com.condoapp.bloc.auth.repository.ContaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService{
+
+    private ContaRepository contaRepository;
+    private JwtService jwtService;
+
+    @Override
+    public LoginResponseDTO logar(LoginRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public Void registrar(RegisterRequestDTO dto) {
+        return null;
+    }
 }
