e76d6b98529942ce48fd87ddc758964f806544f5
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-27 20:33:04 -0300

    feat: adição do dto de registro

--- src/main/java/com/condoapp/bloc/auth/dto/RegisterRequestDTO.java
+++ src/main/java/com/condoapp/bloc/auth/dto/RegisterRequestDTO.java
@@ -1,4 +1,24 @@
 package com.condoapp.bloc.auth.dto;
 
+import com.condoapp.bloc.auth.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequestDTO {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+    @NotBlank
+    private Role tipoUsuario;
 }
