f9f476de16b3f620708d24a53d68dad435c80ec5
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-27 19:37:04 -0300

    feat: interface para controle do serviço

--- src/main/java/com/condoapp/bloc/auth/service/AuthService.java
+++ src/main/java/com/condoapp/bloc/auth/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.condoapp.bloc.auth.service;
 
+import com.condoapp.bloc.auth.dto.LoginRequestDTO;
+import com.condoapp.bloc.auth.dto.LoginResponseDTO;
+import com.condoapp.bloc.auth.dto.RegisterRequestDTO;
+
 public interface AuthService {
+    LoginResponseDTO logar(LoginRequestDTO dto);
+    Void registrar(RegisterRequestDTO dto);
 }
