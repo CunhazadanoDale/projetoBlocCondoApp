49d6793d153a072ba1b0aa0085d0c538f606fd2e
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-27 20:18:20 -0300

    feat: implementado o registro elogin de usuarios

--- src/main/java/com/condoapp/bloc/auth/controller/AuthController.java
+++ src/main/java/com/condoapp/bloc/auth/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.condoapp.bloc.auth.controller;
 
 import com.condoapp.bloc.auth.dto.LoginRequestDTO;
 import com.condoapp.bloc.auth.dto.LoginResponseDTO;
+import com.condoapp.bloc.auth.dto.RegisterRequestDTO;
+import com.condoapp.bloc.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login (@RequestBody LoginRequestDTO loginRequestDTO) {
-        return ResponseEntity.ok()
+    public ResponseEntity<LoginResponseDTO> login (@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.ok(authService.logar(loginRequestDTO));
+    }
+
+
+    @PostMapping("/registrar")
+    public ResponseEntity<LoginResponseDTO> register (@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
+        return ResponseEntity.ok(authService.registrar(registerRequestDTO));
     }
 }
