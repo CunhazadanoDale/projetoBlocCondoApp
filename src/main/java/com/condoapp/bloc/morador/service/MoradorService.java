6a335a75f1b039bfcfd37e2f922416b5e8d1d67c
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-28 20:28:43 -0300

    feat: primeiros dois metodos do service de morador

--- src/main/java/com/condoapp/bloc/morador/service/MoradorService.java
+++ src/main/java/com/condoapp/bloc/morador/service/MoradorService.java
@@ -1,4 +1,10 @@
 package com.condoapp.bloc.morador.service;
 
+import com.condoapp.bloc.auth.entity.Conta;
+
+import java.util.UUID;
+
 public interface MoradorService {
+    MoradorResponseDTO completarPerfil(Conta conta, MoradorRequestDTO dto);
+    MoradorResponseDTO buscarPorUUID (UUID uuid);
 }
