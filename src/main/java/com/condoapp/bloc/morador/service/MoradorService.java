b3fd861152a595905575d0f6502f54e9a18f65d8
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-28 20:42:36 -0300

    feat: imports de dto adicionados

--- src/main/java/com/condoapp/bloc/morador/service/MoradorService.java
+++ src/main/java/com/condoapp/bloc/morador/service/MoradorService.java
@@ -1,6 +1,8 @@
 package com.condoapp.bloc.morador.service;
 
 import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.morador.dto.MoradorRequestDTO;
+import com.condoapp.bloc.morador.dto.MoradorResponseDTO;
 
 import java.util.UUID;
 
