f436b8f3b0fcb165b1ea11fd14d6b019f1ba6e2f
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-28 21:00:30 -0300

    feat: implementado busca por uuid, resta apenas o completar perfil

--- src/main/java/com/condoapp/bloc/morador/service/MoradorServiceImpl.java
+++ src/main/java/com/condoapp/bloc/morador/service/MoradorServiceImpl.java
@@ -1,4 +1,46 @@
 package com.condoapp.bloc.morador.service;
 
-public class MoradorServiceImpl {
+import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.morador.dto.MoradorRequestDTO;
+import com.condoapp.bloc.morador.dto.MoradorResponseDTO;
+import com.condoapp.bloc.morador.entity.Morador;
+import com.condoapp.bloc.morador.repository.MoradorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MoradorServiceImpl implements MoradorService{
+
+    private final MoradorRepository moradorRepository;
+    private final CondominioRepository condominioRepository;
+
+    @Override
+    public MoradorResponseDTO completarPerfil(Conta conta, MoradorRequestDTO dto) {
+        Morador morador = Morador.builder()
+                .nomeCompleto(dto.getNomeCompleto())
+                .unidade(dto.getUnidade())
+                .telefone(dto.getTelefone())
+                .bloco(dto.getBloco())
+                .condominio(dto.getCondominioId())
+                .build();
+    }
+
+    @Override
+    public MoradorResponseDTO buscarPorUUID(UUID uuid) {
+        Morador morador = moradorRepository.findByUUID(uuid)
+                .orElseThrow(() -> new RuntimeException("Nenhum morador encontrado"));
+
+        return MoradorResponseDTO.builder()
+                .moradorUUID(morador.getUuid())
+                .nomeCompleto(morador.getNomeCompleto())
+                .unidade(morador.getUnidade())
+                .telefone(morador.getTelefone())
+                .bloco(morador.getBloco())
+                .condominioId(morador.getCondominio().getCondominioId())
+                .criadoEm(morador.getCriadoEm())
+                .build();
+    }
 }
