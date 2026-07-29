822a2870a7a2e6e4667788e80c4b235b5e14fcbe
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-28 21:00:08 -0300

    feat: metodo para busca por uuid

--- src/main/java/com/condoapp/bloc/morador/repository/MoradorRepository.java
+++ src/main/java/com/condoapp/bloc/morador/repository/MoradorRepository.java
@@ -4,6 +4,11 @@ import com.condoapp.bloc.morador.entity.Morador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface MoradorRepository extends JpaRepository<Morador, Long> {
+
+    Optional<Morador> findByUUID(UUID uuid);
 }
