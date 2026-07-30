23162374a71bd3018f142c406637cb6d8b9f68c0
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-30 20:32:59 -0300

    feat: criador do mapper para evitar repetição de codigo

--- src/main/java/com/condoapp/bloc/morador/dto/MoradorMapper.java
+++ src/main/java/com/condoapp/bloc/morador/dto/MoradorMapper.java
@@ -1,4 +1,18 @@
 package com.condoapp.bloc.morador.dto;
 
+import com.condoapp.bloc.morador.entity.Morador;
+
 public class MoradorMapper {
+
+    public static MoradorResponseDTO fromEntityToResponse(Morador morador) {
+        return MoradorResponseDTO.builder()
+                .moradorUUID(morador.getUuid())
+                .nomeCompleto(morador.getNomeCompleto())
+                .unidade(morador.getUnidade())
+                .bloco(morador.getBloco())
+                .telefone(morador.getTelefone())
+                .condominioUUID(morador.getCondominio().getUuid())
+                .criadoEm(morador.getCriadoEm())
+                .build();
+    }
 }
