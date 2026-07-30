585bea37b512aa987c7cfc3cf93a6fe0a7074244
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-30 20:53:15 -0300

    feat: completo a adiçao docontroller

--- src/main/java/com/condoapp/bloc/morador/controller/MoradorController.java
+++ src/main/java/com/condoapp/bloc/morador/controller/MoradorController.java
@@ -1,4 +1,44 @@
 package com.condoapp.bloc.morador.controller;
 
+import com.condoapp.bloc.auth.entity.Conta;
+import com.condoapp.bloc.morador.dto.MoradorRequestDTO;
+import com.condoapp.bloc.morador.dto.MoradorResponseDTO;
+import com.condoapp.bloc.morador.service.MoradorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/moradores")
+@RequiredArgsConstructor
 public class MoradorController {
+
+    private final MoradorService moradorService;
+
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<MoradorResponseDTO> buscarPorUUID(@PathVariable UUID uuid) {
+        return ResponseEntity.ok(moradorService.buscarPorUUID(uuid));
+    }
+
+    @PostMapping()
+    public ResponseEntity<MoradorResponseDTO> completarPerfil(@Valid @RequestBody MoradorRequestDTO moradorRequestDTO,
+                                                              @AuthenticationPrincipal Conta conta,
+                                                              UriComponentsBuilder uriBuilder) {
+        MoradorResponseDTO morador = moradorService.completarPerfil(conta, moradorRequestDTO);
+
+        URI uri = uriBuilder.path("/moradores/{uuid}")
+                .buildAndExpand(morador.getMoradorUUID())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(morador);
+    }
+
 }
