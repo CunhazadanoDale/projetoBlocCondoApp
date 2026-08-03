ae6a216e8bb447bcf1011c8e1cc841fcbfd45b30
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-03 19:41:18 -0300

    feat: request realizado com sucesso

--- src/main/java/com/condoapp/bloc/prestador/dto/PrestadorRequestDTO.java
+++ src/main/java/com/condoapp/bloc/prestador/dto/PrestadorRequestDTO.java
@@ -1,4 +1,22 @@
 package com.condoapp.bloc.prestador.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PrestadorRequestDTO {
+
+    @NotBlank
+    private String nomeCompleto;
+
+    @NotBlank
+    private String cpfOuCnpj;
+    private String telefone;
+    private String descricao;
 }
