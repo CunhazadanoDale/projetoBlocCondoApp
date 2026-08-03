57d43bb14c7a095ee3dba5694bbf5361bd04b0cc
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-03 20:23:15 -0300

    feat: metodo para buscar por uuid

--- src/main/java/com/condoapp/bloc/prestador/repository/PrestadorRepository.java
+++ src/main/java/com/condoapp/bloc/prestador/repository/PrestadorRepository.java
@@ -4,8 +4,12 @@ import com.condoapp.bloc.prestador.entity.Prestador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface PrestadorRepository extends JpaRepository<Prestador, Long> {
 
+    Optional<Prestador> findByUUID(UUID uuid);
     boolean existsByCpfOuCnpj(String cpfOuCnpj);
 }
