6773ed9e52da9112dbf95be4da718a0ced3e1616
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-03 19:58:18 -0300

    feat: repository com checagem de cpf ou cnpj

--- src/main/java/com/condoapp/bloc/prestador/repository/PrestadorRepository.java
+++ src/main/java/com/condoapp/bloc/prestador/repository/PrestadorRepository.java
@@ -1,4 +1,11 @@
 package com.condoapp.bloc.prestador.repository;
 
-public interface PrestadorRepository {
+import com.condoapp.bloc.prestador.entity.Prestador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrestadorRepository extends JpaRepository<Prestador, Long> {
+
+    boolean existsByCpfOuCnpj(String cpfOuCnpj);
 }
