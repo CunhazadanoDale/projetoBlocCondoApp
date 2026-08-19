6919c1d03b9e945e55e0d45d58ee976f6ac66671
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-18 22:50:03 -0300

    feat: metodo para busca de ativos por condominioID

--- src/main/java/com/condoapp/bloc/agendamento/repository/EspacoRepository.java
+++ src/main/java/com/condoapp/bloc/agendamento/repository/EspacoRepository.java
@@ -1,9 +1,12 @@
 package com.condoapp.bloc.agendamento.repository;
 
 import com.condoapp.bloc.agendamento.entity.Espaco;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +14,7 @@ import java.util.UUID;
 public interface EspacoRepository extends JpaRepository<Espaco, Long> {
 
     Optional<Espaco> findByUuid(UUID uuid);
+
+    @Query("SELECT e Espaco e WHERE e.condominio.condominio_id = :condominioID AND e.ativo = true")
+    List<Espaco> findAllActiveByCondominioID(@Param("condominioID") Long condominioID);
 }
