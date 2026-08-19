5b818a9862af78b98c9e53318b352d4cf82c9f29
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-18 22:44:35 -0300

    refact: trocado formato de resposta para o formato ideal com DTO

--- src/main/java/com/condoapp/bloc/agendamento/service/EspacoService.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/EspacoService.java
@@ -1,5 +1,7 @@
 package com.condoapp.bloc.agendamento.service;
 
+import com.condoapp.bloc.agendamento.dto.EspacoRequestDTO;
+import com.condoapp.bloc.agendamento.dto.EspacoResponseDTO;
 import com.condoapp.bloc.agendamento.entity.Agendamento;
 import com.condoapp.bloc.agendamento.entity.Espaco;
 
@@ -8,7 +10,7 @@ import java.util.List;
 
 public interface EspacoService {
 
-    List<Espaco> listarEspacos();
-    Espaco criarEspaco(Espaco espaco);
-    Espaco atualizarEspaco(Long espacoId, Espaco espaco);
+    List<EspacoResponseDTO> listarEspacos();
+    EspacoResponseDTO criarEspaco(Espaco espaco);
+    EspacoResponseDTO atualizarEspaco(Long espacoId, EspacoRequestDTO espaco);
 }
--- src/main/java/com/condoapp/bloc/agendamento/service/EspacoServiceImpl.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/EspacoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.condoapp.bloc.agendamento.service;
 
-import com.condoapp.bloc.agendamento.entity.Agendamento;
+import com.condoapp.bloc.agendamento.dto.EspacoRequestDTO;
+import com.condoapp.bloc.agendamento.dto.EspacoResponseDTO;
 import com.condoapp.bloc.agendamento.entity.Espaco;
 import com.condoapp.bloc.agendamento.repository.EspacoRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +17,22 @@ public class EspacoServiceImpl implements EspacoService {
     private final EspacoRepository espacoRepository;
 
     @Override
-    public List<Espaco> listarEspacos() {
+    public List<EspacoResponseDTO> listarEspacos() {
         return List.of();
     }
 
     @Override
-    public Espaco criarEspaco(Espaco espaco) {
+    public EspacoResponseDTO criarEspaco(Espaco espaco) {
         return espacoRepository.save(espaco);
     }
 
     @Override
-    public Espaco atualizarEspaco(Long espacoId, Espaco espaco) {
+    public EspacoResponseDTO atualizarEspaco(Long espacoId, EspacoRequestDTO espaco) {
         Espaco espacoDoBancoDeDados = espacoRepository.findById(espacoId)
                 .orElseThrow(() -> new RuntimeException("Espaco não encontrado"));
 
         espacoDoBancoDeDados.setDescricao(espaco.getDescricao());
         espacoDoBancoDeDados.setLimiteReservaSemana(espaco.getLimiteReservaSemana());
-        espacoDoBancoDeDados.setVersion(espaco.getVersion());
 
         return espacoRepository.save(espacoDoBancoDeDados);
     }
