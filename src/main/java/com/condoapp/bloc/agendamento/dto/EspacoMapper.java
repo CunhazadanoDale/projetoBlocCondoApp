ec7d44676ef9c95f47026c51c87c1287b555e8b5
CunhazadaNoDale <cunha12324@gmail.com>
2026-08-18 22:42:52 -0300

    feat: adição de um mapper para o espaco

--- src/main/java/com/condoapp/bloc/agendamento/dto/EspacoMapper.java
+++ src/main/java/com/condoapp/bloc/agendamento/dto/EspacoMapper.java
@@ -1,4 +1,20 @@
 package com.condoapp.bloc.agendamento.dto;
 
+import com.condoapp.bloc.agendamento.entity.Espaco;
+
 public class EspacoMapper {
+
+    public static EspacoResponseDTO fromEntityToResponse(Espaco espaco) {
+        return EspacoResponseDTO.builder()
+                .uuid(espaco.getUuid())
+                .condominioID(espaco.getCondominio().getCondominioId())
+                .nome(espaco.getNome())
+                .descricao(espaco.getDescricao())
+                .capacidade(espaco.getCapacidade())
+                .minHoras(espaco.getAntecedenciaMinHoras())
+                .cancelamentoMinHoras(espaco.getCancelamentoMinHoras())
+                .limiteReservaSemana(espaco.getLimiteReservaSemana())
+                .ativo(espaco.isAtivo())
+                .build();
+    }
 }
