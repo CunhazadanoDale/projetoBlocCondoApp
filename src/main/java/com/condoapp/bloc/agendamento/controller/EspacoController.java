9d447ee551b4cae6ee496bf3b9c0f2a2af05d57d
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-01 21:47:46 -0300

    refact: controller totalmente refeito depois de uma grande varredura de DTO e ações. resta apenas o metodo de agendamento, que sera o proximo modulo tocado

--- src/main/java/com/condoapp/bloc/agendamento/controller/EspacoController.java
+++ src/main/java/com/condoapp/bloc/agendamento/controller/EspacoController.java
@@ -1,12 +1,17 @@
 package com.condoapp.bloc.agendamento.controller;
 
+import com.condoapp.bloc.agendamento.dto.EspacoRequestDTO;
+import com.condoapp.bloc.agendamento.dto.EspacoResponseDTO;
 import com.condoapp.bloc.agendamento.entity.Agendamento;
 import com.condoapp.bloc.agendamento.entity.Espaco;
 import com.condoapp.bloc.agendamento.service.AgendamentoService;
 import com.condoapp.bloc.agendamento.service.EspacoService;
+import com.condoapp.bloc.auth.entity.Conta;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -22,26 +27,25 @@ public class EspacoController {
     private final AgendamentoService agendamentoService;
 
     @GetMapping("/{uuid}/espacos")
-    public ResponseEntity<List<Espaco>> listarEspacosDoCondominio(@PathVariable(name = "uuid") UUID condominioUuid) {
-        return new ResponseEntity<>(espacoService.listarEspacos(), HttpStatus.OK);
+    public ResponseEntity<List<EspacoResponseDTO>> listarEspacosDoCondominio(@PathVariable(name = "uuid") UUID condominioUuid) {
+        return new ResponseEntity<>(espacoService.listarEspacos(condominioUuid), HttpStatus.OK);
     }
 
-    @PostMapping("/{uuid}/espacos")
-    public ResponseEntity<Espaco> cadastrarEspaco(@PathVariable(name = "uuid") UUID condominioUuid,
-                                                  @RequestBody Espaco espaco) {
-        return new ResponseEntity<>(espacoService.criarEspaco(espaco), HttpStatus.CREATED);
+    @PostMapping("/espacos")
+    public ResponseEntity<EspacoResponseDTO> cadastrarEspaco(@AuthenticationPrincipal Conta conta,
+            @Valid @RequestBody EspacoRequestDTO espaco) {
+        return new ResponseEntity<>(espacoService.criarEspaco(espaco, conta), HttpStatus.CREATED);
     }
 
-    @PutMapping("/{uuid}/espacos/{espacoId}")
-    public ResponseEntity<Espaco> atualizarEspaco(@PathVariable(name = "uuid") UUID condominioUuid,
-                                                  @PathVariable Long espacoId,
-                                                  @RequestBody Espaco espaco) {
-        return new ResponseEntity<>(espacoService.atualizarEspaco(espacoId, espaco), HttpStatus.OK);
+    @PutMapping("/espacos/{espacoId}")
+    public ResponseEntity<EspacoResponseDTO> atualizarEspaco(@PathVariable UUID espacoId,
+                                                  @AuthenticationPrincipal Conta conta,
+                                                  @Valid @RequestBody EspacoRequestDTO espaco) {
+        return new ResponseEntity<>(espacoService.atualizarEspaco(espacoId, espaco, conta), HttpStatus.OK);
     }
 
-    @GetMapping("/{uuid}/espacos/{espacoId}/disponibilidade")
-    public ResponseEntity<List<Agendamento>> listarDisponibilidade(@PathVariable(name = "uuid") UUID condominioUuid,
-                                                                   @PathVariable Long espacoId,
+    @GetMapping("/espacos/{espacoUUID}/disponibilidade")
+    public ResponseEntity<List<Agendamento>> listarDisponibilidade(@PathVariable(name = "espacoUUID") UUID espacoId,
                                                                    @RequestParam LocalDate data) {
         return new ResponseEntity<>(agendamentoService.buscarDisponibilidade(espacoId, data), HttpStatus.OK);
     }
