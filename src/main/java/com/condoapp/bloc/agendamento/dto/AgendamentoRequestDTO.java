281a28ba9cfe045fd5ac60866ed7beeea67229fd
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-02 20:17:27 -0300

    feat: request de agendamento criado completo

--- src/main/java/com/condoapp/bloc/agendamento/dto/AgendamentoRequestDTO.java
+++ src/main/java/com/condoapp/bloc/agendamento/dto/AgendamentoRequestDTO.java
@@ -1,4 +1,33 @@
 package com.condoapp.bloc.agendamento.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AgendamentoRequestDTO {
+
+    @NotNull
+    private UUID espacoUUID;
+
+    @NotBlank
+    private String nomeResponsavel;
+
+    @NotBlank
+    private String unidadeResponsavel;
+
+    @NotNull
+    private LocalDateTime inicio;
+
+    @NotNull
+    private LocalDateTime fim;
 }
