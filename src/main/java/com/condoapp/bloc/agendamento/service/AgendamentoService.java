80d4cd42ca6cbeca2267273fe54b8d0107bc8049
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-02 20:18:53 -0300

    refact: correção dos parametros de chamada

--- src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
+++ src/main/java/com/condoapp/bloc/agendamento/service/AgendamentoService.java
@@ -1,15 +1,20 @@
 package com.condoapp.bloc.agendamento.service;
 
+import com.condoapp.bloc.agendamento.dto.AgendamentoRequestDTO;
+import com.condoapp.bloc.agendamento.dto.AgendamentoResponseDTO;
+import com.condoapp.bloc.agendamento.dto.AlterarAgendamentoDTO;
 import com.condoapp.bloc.agendamento.entity.Agendamento;
+import com.condoapp.bloc.agendamento.enums.StatusAgendamento;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface AgendamentoService {
-    Agendamento buscarPorUUID(UUID uuid);
-    Agendamento criarAgendamento(Agendamento agendamento);
-    List<Agendamento> listarAgendamentosDeCondominio(UUID condominioId);
+    AgendamentoResponseDTO buscarPorUUID(UUID uuid);
+    AgendamentoResponseDTO criarAgendamento(AgendamentoRequestDTO agendamento);
+    List<AgendamentoResponseDTO> listarAgendamentosDeCondominio(UUID condominioId);
+    AgendamentoResponseDTO alterarAgendamentoStatusEObservacao(UUID agendamentoUUID, AlterarAgendamentoDTO alterarAgendamentoDTO);
     void cancelarAgendamento(UUID uuid);
-    List<Agendamento> buscarDisponibilidade(Long espacoId, LocalDate data);
+    List<AgendamentoResponseDTO> buscarDisponibilidade(Long espacoId, LocalDate data);
 }
