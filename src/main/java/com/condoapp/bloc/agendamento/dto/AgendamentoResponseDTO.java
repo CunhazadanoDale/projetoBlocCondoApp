fd99693ec53d4641ac061d26e8ea76885517fb28
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-02 20:18:20 -0300

    feat: adição da resposta para as buscas e consultas do agendamento

--- src/main/java/com/condoapp/bloc/agendamento/dto/AgendamentoResponseDTO.java
+++ src/main/java/com/condoapp/bloc/agendamento/dto/AgendamentoResponseDTO.java
@@ -1,4 +1,32 @@
 package com.condoapp.bloc.agendamento.dto;
 
+import com.condoapp.bloc.agendamento.enums.StatusAgendamento;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AgendamentoResponseDTO {
+
+    private UUID espacoUUID;
+    private UUID agendamentoUUID;
+    private String nomeResponsavel;
+    private String unidadeResponsavel;
+
+    private StatusAgendamento statusAgendamento;
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
+    private LocalDateTime criadoEm;
+
+    private String observacao;
+
+
+
 }
