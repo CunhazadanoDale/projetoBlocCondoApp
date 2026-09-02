76a4cfbbb8d6e0ccd273e751a6d908ec7d555c17
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-02 20:18:39 -0300

    feat: DTO que visa apenas alterar o status e a observacao do agendamento

--- src/main/java/com/condoapp/bloc/agendamento/dto/AlterarAgendamentoDTO.java
+++ src/main/java/com/condoapp/bloc/agendamento/dto/AlterarAgendamentoDTO.java
@@ -1,4 +1,17 @@
 package com.condoapp.bloc.agendamento.dto;
 
+import com.condoapp.bloc.agendamento.enums.StatusAgendamento;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AlterarAgendamentoDTO {
+
+    @NotNull
+    private StatusAgendamento statusAgendamento;
+    private String observacao;
 }
