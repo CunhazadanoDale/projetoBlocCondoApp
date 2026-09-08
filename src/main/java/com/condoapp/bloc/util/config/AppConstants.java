70b7e32b9cabca6d78bfb5553652e896ecc24a00
CunhazadaNoDale <cunha12324@gmail.com>
2026-09-08 19:59:24 -0300

    feat: adição de constantes para paginação

---
+++ src/main/java/com/condoapp/bloc/util/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.condoapp.bloc.util.config;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "50";
+    public static final String SORT_AGENDAMENTOS_BY = "agendamentosId";
+    public static final String SORT_DIR = "asc";
+}
